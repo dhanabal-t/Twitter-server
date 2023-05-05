4065d85a98695366c111a6988cb1e85b80f13c6b
Dhanabal T <dhanabalt48@gmail.com>
2023-05-05 15:24:42 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+const authRoute = require('./routes/Auth');
+const usersRoute = require('./routes/Users');
+const tweetsRoute = require('./routes/Tweets');
+
+// require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// const uri = process.env.ATLAS_URI;
+mongoose.connect("mongodb+srv://admin:admin@cluster0.apdgkeo.mongodb.net/?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true });
+const connection = mongoose.connection;
+connection.once('open', () => {
+  console.log('MongoDB database connection established successfully');
+});
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/auth', authRoute);
+app.use('/api/users', usersRoute);
+app.use('/api/tweets', tweetsRoute);
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
